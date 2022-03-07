522d572bd42cb7912982bbb5d4a04632e3913a4c
jguibrandao <jguibrandao@gmail.com>
2022-03-07 10:15:23 -0300

    setting validations and creating user with encrypted password

--- src/controllers/UserController/index.js
+++ src/controllers/UserController/index.js
@@ -1,10 +1,33 @@
+const Auth = require("../../middlewares/auth")
+const User = require("../../models/User")
+
 class UserController {
     async register(req, res) {
         const { name, email, password, confirmpassword } = req.body
+        const userExists = await User.findOne({ email: email })
 
-        if(!name) {
-            return res.status(401).json({msg: 'Missing name'})
+        if(userExists) {
+            return res.status(401).json({msg: 'User already exists'})
         }
+          
+        if(confirmpassword != password) {
+            return res.status(401).json({msg: 'Diff passwords'})
+        }
+
+        const passwordHash = await Auth.createPassword(password)
+
+        try {
+            const newUser = await User.create({
+                name,
+                email,
+                password: passwordHash
+            })
+            return res.status(201).json({msg: 'User created'})
+        } catch (error) {
+            return res.status(400).json(error)
+        }
+        
+        
     }
 }
 
