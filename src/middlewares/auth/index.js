4d986ff42d40471256df2b00fb9411bf8a3c77ec
jguibrandao <jguibrandao@gmail.com>
2022-03-07 16:02:49 -0300

    check token (auth) method

--- src/middlewares/auth/index.js
+++ src/middlewares/auth/index.js
@@ -30,6 +30,24 @@ const Auth = {
             return res.status(401).json(err)
         }
        
+    },
+
+    checkToken(req, res, next) {
+        const authHeader = req.headers['authorization']
+        const token = authHeader && authHeader.split(" ")[1]
+
+        if(!token) {
+            return res.status(401).json({msg: 'No allowed / missing token'})
+        }
+
+        try {
+            const secret = process.env.SECRET
+            jwt.verify(token, secret)
+
+            next()
+        } catch (error) {
+            return res.status(400).json({msg: 'Invalid Token'})
+        }
     }
 }
     
