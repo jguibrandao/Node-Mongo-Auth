2d24d83cdb22872499a0ed13fa1e8a0e1db625e2
jguibrandao <jguibrandao@gmail.com>
2022-03-06 22:15:46 -0300

    setting default route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,14 @@ const UserController = require("./controllers/userController")
 
 const routes = require("express").Router()
 
+routes.get('/', (req, res) => {
+    try {
+        return res.status(200).json('salve')
+    } catch (error) {
+        return res.status(400).json(error)
+    }
+})
+
 routes.post('/auth/register', UserController.register)
 
 module.exports = routes
