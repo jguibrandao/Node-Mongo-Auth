9b6fb0e2bcc1242258bd79401d7596b800dd5529
jguibrandao <jguibrandao@gmail.com>
2022-03-07 10:14:52 -0300

    setting log when server starts

--- src/server.js
+++ src/server.js
@@ -2,4 +2,4 @@ const connect = require('./db')
 const app = require('./config/app')
 
 connect()
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, console.log('rodando'));
