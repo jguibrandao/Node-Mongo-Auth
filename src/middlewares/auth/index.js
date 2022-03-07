789fc8e9bf1dcf67e8dea6f5cc727ff40814aa5b
jguibrandao <jguibrandao@gmail.com>
2022-03-07 14:59:34 -0300

    creating check password and authLogin methods

--- src/middlewares/auth/index.js
+++ src/middlewares/auth/index.js
@@ -1,10 +1,35 @@
+require('dotenv').config()
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const Auth = {
     async createPassword(password) {
         const salt = await bcrypt.genSalt(12)
         const passwordHash = await bcrypt.hash(password, salt)
         return passwordHash
+    },
+
+    async checkPassword(password, userPassword) {
+        const checkPassword = await bcrypt.compare(password, userPassword)
+        return checkPassword
+    },
+
+    async authLogin(user, res) {
+        try {
+            const secret = process.env.SECRET
+
+            const token = jwt.sign(
+                {
+                id: user._id,
+                },
+                secret,
+            )
+
+            return res.status(200).json({ msg: 'sucessful login', token, user_id: user._id })
+        } catch (err) {
+            return res.status(401).json(err)
+        }
+       
     }
 }
     
